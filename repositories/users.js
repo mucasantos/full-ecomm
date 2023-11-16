7171ed1773df475502d29e7d28327d90c364cacb
HellersonMurilo <calabianquimurilo328@gmail.com>
2023-11-16 19:45:40 -0300

    melhoria

--- repositories/users.js
+++ repositories/users.js
@@ -1,6 +1,18 @@
 const Repository = require("./repository");
 
+const fs = require('fs')
+const crypto = require("crypto")
+
+//criptografia
+//1° passo
+const util = require("util")
+
+const script = util.promisify(crypto.scrypt)
+
 class UserRepository extends Repository {
+
+    apenasUser(){}
+
     async create(atributos) {
 
         console.log(atributos)
